e10bd23a312ced794c9a74e6d3d64aa4053839c6
Emry Brisky <eabrisky@gmail.com>
2021-07-01 20:58:24 -0500

    committing

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
 
+// css
+import './UserPage.css';
+
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
